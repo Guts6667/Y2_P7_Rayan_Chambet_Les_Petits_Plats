550fcd6dd13da3a49a960b2eeb9087d62fea52f9
Rayan <rayanchambet1@gmail.com>
2022-03-22 19:01:17 +0000

    V.0
    Creation of class Recipe

---
+++ script/Models/Recipe.js
@@ -0,0 +1,17 @@
+class Recipe {
+
+    constructor(data){
+        this.id = data.id;
+        this.name = data.name;
+        this.servings = data.servings;
+        this.ingredients = data.ingredients;
+        this.time = data.time;
+        this.description = data.description;
+        this.appliance = data.appliance;
+        this.ustensils = data.ustensils;
+    }
+
+    createRecipeCard(){
+        
+    }
+}
