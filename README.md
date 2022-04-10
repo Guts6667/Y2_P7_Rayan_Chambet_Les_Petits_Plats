ddc2221b52476a06fc98410ebba4011fb2587200
Rayan <rayanchambet1@gmail.com>
2022-04-10 15:20:03 +0100

    V.0.6.1
    Structure ✅
    Display recipes datas  ✅
    Filter by searchbar ✅
    Update recipes results ✅
    Display Tags   ✅
    Add Style + Search with tags

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Y2_P7_Rayan_Chambet_Les_Petits_Plats
+
+
+Régler l'alignement des éléments tags
--- script/utils/fillDropbox.js
+++ script/utils/fillDropbox.js
@@ -1,3 +1,5 @@
+
+
 // Aside: Contains all of the ingredients.
 let asideIngredients = document.querySelector('#asideIngredients')
 // IngredientLists: <ul>
@@ -7,6 +9,18 @@ let arrowUpIngredients = document.querySelector('#arrowUpIngredients');
 // Chevron Down
 let arrowDownIngredients = document.querySelector('#arrowDownIngredients');
 
+// Appliances
+let asideAppliances = document.querySelector('#asideAppliances');
+let arrowUpAppliances = document.querySelector('#arrowUpAppliances');
+let arrowDownAppliances = document.querySelector('#arrowDownAppliances');
+let appliancesResults = document.querySelector('.appliancesResults');
+
+// Ustensils
+let asideUstensils = document.querySelector('#asideUstensils');
+let arrowUpUstensils = document.querySelector('#arrowUpUstensils');
+let arrowDownUstensils = document.querySelector('#arrowDownUstensils');
+let ustensilsResults = document.querySelector('.ustensilsResults');
+//------------------------------------------------------------------------------------
 // When the chevron 'Up' is clicked, calls pushIngredients()
 arrowUpIngredients.addEventListener('click', (e) => {
     e.preventDefault();
@@ -15,10 +29,29 @@ arrowUpIngredients.addEventListener('click', (e) => {
 // When the chevron 'Down' is clicked, calls hideIngredients()
 arrowDownIngredients.addEventListener('click', (e) => {
     e.preventDefault()
-    console.log(e.target);
     hideIngredients()
 })
 
+arrowUpAppliances.addEventListener('click', (e) => {
+    e.preventDefault();
+    pushAppliances();
+})
+
+arrowDownAppliances.addEventListener('click', (e) => {
+    e.preventDefault();
+    hideAppliances();
+})
+
+arrowUpUstensils.addEventListener('click', (e) => {
+    e.preventDefault();
+    pushUstensils();
+})
+
+arrowDownUstensils.addEventListener('click', (e) => {
+    e.preventDefault();
+    hideUstensils();
+})
+//------------------------------------------------------------------------------------
 let allIngredients = [];
 
 // Function pushIngredients: Display all ingredients
@@ -38,15 +71,16 @@ const pushIngredient = () => {
     arrowUpIngredients.classList.add('hidden');
     arrowDownIngredients.classList.remove('hidden');
 
-    let tempInnerHTML;
+    let tempInnerHTMLIngre;
     allIngredients.forEach(ingredient => {
         
-        tempInnerHTML += `<li class = 'ingredientTag pointer'>${ingredient}</li> `
+        tempInnerHTMLIngre += `<li class = 'ingredientTag pointer'>${ingredient}</li> `
     })
 
-    ingredientList.innerHTML = tempInnerHTML;
+    ingredientList.innerHTML = tempInnerHTMLIngre;
 }
 
+//------------------------------------------------------------------------------------
 // Function hideIngredients: Hide all ingredients
 const hideIngredients = ()=> {
     asideIngredients.classList.add('hidden');
@@ -55,4 +89,74 @@ const hideIngredients = ()=> {
 }
 
 
+//------------------------------------------------------------------------------------
+// Functions for Appliances
+let allAppliances = [];
 
+const pushAppliances = () => {
+
+    currentRecipes.forEach(recipe => {
+        allAppliances.push(recipe.appliance);
+    })
+
+    allAppliances = [...new Set([...allAppliances])].sort()
+    
+
+    asideAppliances.classList.remove('hidden')
+    arrowUpAppliances.classList.add('hidden');
+    arrowDownAppliances.classList.remove('hidden');
+
+    let tempInnerHTMLAppliance;
+    allAppliances.forEach(appliance => {
+        
+        tempInnerHTMLAppliance += `<li class = 'applianceTag pointer'>${appliance}</li> `
+    })
+    
+
+    appliancesResults.innerHTML = tempInnerHTMLAppliance;
+}
+
+const hideAppliances = ()=> {
+    asideAppliances.classList.add('hidden');
+    arrowDownAppliances.classList.add('hidden');
+    arrowUpAppliances.classList.remove('hidden')
+}
+
+
+//------------------------------------------------------------------------------------
+// Functions for Ustensils
+let allUstensils = [];
+
+const pushUstensils = () => {
+
+    currentRecipes.forEach(recipe => {
+        
+        allUstensils.push(recipe.ustensils)
+    })
+    // allUstensils.map(ustensils => ustensils)
+
+    allUstensils = allUstensils.flatMap(ustensils => ustensils)
+    allUstensils = [...new Set([...allUstensils])].sort()
+    console.log(allUstensils);
+    
+
+    asideUstensils.classList.remove('hidden')
+    arrowUpUstensils.classList.add('hidden');
+    arrowDownUstensils.classList.remove('hidden');
+
+    let tempInnerHTMLUstensils;
+    allUstensils.forEach(ustensil => {
+        
+        tempInnerHTMLUstensils += `<li class = 'ustensilsTag pointer'>${ustensil}</li> `
+    })
+    
+
+    ustensilsResults.innerHTML = tempInnerHTMLUstensils;
+}
+
+
+const hideUstensils = ()=> {
+    asideUstensils.classList.add('hidden');
+    arrowDownUstensils.classList.add('hidden');
+    arrowUpUstensils.classList.remove('hidden')
+}
