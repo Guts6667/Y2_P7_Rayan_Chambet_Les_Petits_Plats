300076efe3f277c17e6ae4dca005582d29d268e2
Rayan <rayanchambet1@gmail.com>
2022-05-03 15:37:46 +0200

    V.1(Algo#2)
    Replaced function some( ) by a forEach loop

--- script/utils/searchbar.js
+++ script/utils/searchbar.js
@@ -17,28 +17,54 @@ const detectSearchInput = () => {
 };
 
 // -----------------------------------------------------------------------------------------------------
-// Algorithme #2
-// Second version of the filtering function through the main searchbar
+// Filter when typing in searchbar (Utiliser filter pour trouver TOUTES les valeurs correspondantes)
+// Algo 1
+
+// const searchFilter = () => {
+//   currentRecipes = recipes.filter(
+//      (recipe) =>
+//        recipe.name.toLowerCase().includes(searchbar.value.toLowerCase()) ||
+//        recipe.description
+//          .toLowerCase()
+//          .includes(searchbar.value.toLowerCase()) ||
+//        recipe.ingredients.some((ingredient) =>
+//          ingredient.ingredient
+//            .toLowerCase()
+//            .includes(searchbar.value.toLowerCase())
+//        )
+//    );
+  
+//    updateIngredients()
+//    tagUpdate();
+//    displayRecipeCards()
+//  };
 
 
+// Empty currentRecipes[] then uses a for loop and a forEach loop to check every values of the array.
+// If one of the object's value matches the search bar's value, it pushes the recipes in currentRecipes[]
+// Algo 2
 const searchFilter = () => {
-  currentRecipes = [];
 
-for(let i= 0;  i < recipes.length; i++){
-    if(recipes[i].name.toLowerCase().includes(searchbar.value.toLowerCase()) || 
-    recipes[i].description.toLowerCase().includes(searchbar.value.toLowerCase()) ||
-          recipes[i].ingredients.some((ingredient) =>
-            ingredient.ingredient
-              .toLowerCase()
-              .includes(searchbar.value.toLowerCase()))){
-                  currentRecipes.push(recipes[i]);
-              }
-}
+  currentRecipes = [];
+  for(let i = 0; i < recipes.length; i++){
+
+    if(recipes[i].name.toLowerCase().includes(searchbar.value.toLowerCase()) ||
+      recipes[i].description.toLowerCase().includes(searchbar.value.toLowerCase()) ||
+      recipes[i].ingredients.forEach(ingredient => { ingredient.ingredient.toLowerCase().includes(searchbar.value.toLowerCase())
+          currentRecipes.push(recipes[i])
+      })
+    
+    );
+  }
+  
+   updateIngredients()
+   tagUpdate();
+   displayRecipeCards()
+ };
+
+
  
-  updateIngredients()
-  tagUpdate();
-  displayRecipeCards()
-};
+
 // -----------------------------------------------------------------------------------------------------
 
 const searchbarFunc = () => {
